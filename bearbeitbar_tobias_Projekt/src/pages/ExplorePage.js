8ef546e13b9c7baf25890c8696b4d8ec02335279
TobiasMaximilianAlexandreBasten <28989330+TobiasMaximilianAlexandreBasten@users.noreply.github.com>
2021-06-01 15:15:14 +0200

    fixes logoicon

--- bearbeitbar_tobias_Projekt/src/pages/ExplorePage.js
+++ bearbeitbar_tobias_Projekt/src/pages/ExplorePage.js
@@ -3,7 +3,8 @@ import {Link} from 'react-router-dom'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from '../components/Button'; 
+import Button from '../components/Button';
+import Picture from '../components/Picture';
 
 function ExplorePage() {
   return (
--- bearbeitbar_tobias_Projekt/src/pages/UserRegistrationPage.js
+++ bearbeitbar_tobias_Projekt/src/pages/UserRegistrationPage.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from '../components/Button';
+import Picture from '../components/Picture';
 
 function UserRegistrationPage() {
   return (
