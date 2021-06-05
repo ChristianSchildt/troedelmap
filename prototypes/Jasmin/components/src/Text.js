f4244f300683627b9db7358a7adc0632f1d4701f
JasminBreemann <73950301+JasminBreemann@users.noreply.github.com>
2021-06-05 14:50:17 +0200

    Components
    Done aside from styling as far as I can tell

---
+++ prototypes/Jasmin/components/src/Text.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Text extends React.Component{
+
+    state = {
+        text: " "
+    }
+    constructor(props) {
+        super(props)
+        }
+    render(){
+        return(
+            <text
+                id={this.props.id}
+                value={this.props.value}
+            />
+        )
+    }
+
+
+}
+
+export default Text;
