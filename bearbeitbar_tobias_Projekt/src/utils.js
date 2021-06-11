1c96ea68f17069e101d353542c1931bf8f64032b
Christian Schildt <38693641+christian-schildt@users.noreply.github.com>
2021-06-11 12:30:21 +0200

    added possibility to query the geographical coordinates of an address

---
+++ bearbeitbar_tobias_Projekt/src/utils.js
@@ -0,0 +1,19 @@
+export function callAjax(url, callback) {
+    var xmlhttp;
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+            callback(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+export function adressToLatLng(address, callback) {
+    callAjax('https://nominatim.openstreetmap.org/search?format=json&q='+encodeURIComponent(address), function(data) {
+        var parsedData = JSON.parse(data)
+        // wir nehmen erst mal an, dass das Ergebnis eindeutig ist.
+        callback([parseFloat(parsedData[0].lat), parseFloat(parsedData[0].lon)]);
+    })
+}
